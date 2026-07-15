94d4bf39d81eca5ec7970d2f59c4c2e765a92686
Bantonio12 <thcha0727@gmail.com>
2026-07-15 17:56:20 +0900

    Added Temp State enum for robot states

---
+++ src/robotStates.js
@@ -0,0 +1,9 @@
+export const ROBOT_STATE = {
+  IDLE: "idle",
+  APPROACH: "approach",
+  ATTACK: "attack", // 나중에 좀 더 세분화 할것
+  DODGE: "dodge",
+  BLOCK: "block",
+  STAGGERED: "staggered", // 스턴 맞은 상태
+  DESTROYED: "destroyed",
+};
