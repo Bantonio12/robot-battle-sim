b650886ed4b7048103be8169b3b39b3909bccc9a
Bantonio12 <thcha0727@gmail.com>
2026-07-16 10:38:30 +0900

    viewport module modularization/viewport module 추출

--- src/scene/camera.js
+++ src/scene/camera.js
@@ -1,1 +1,9 @@
-// TODO
+// 카메라 설정
+import * as THREE from 'three';
+
+export function createCamera() {
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 2, 5); // 캐릭터를 잘 볼 수 있도록 카메라 위치 조정
+    camera.lookAt(0, 1, 0); // 캐릭터가 있는 위치를 바라보도록 설정
+    return camera;
+}
